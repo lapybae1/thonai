3579bfeca288c1f26a1245dd465308188d722dc4
lapybae1 <153311024+lapybae1@users.noreply.github.com>
2023-12-08 23:57:34 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Trang(^)';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100035786240845';
+const eventName = 'Happy Birthday🎂 ';
+const titleCard = 'Winter not cold🌨  ';
+const contentCard = 'Happy birthday to Thỏ nai. Chúc cho bạn thỏ của tớ 9/12 tràn ngập niềm vui và những nụ cười. Wish all the good will come to you.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'trang1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
